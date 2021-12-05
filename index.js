30a4e7361d807018003591e38e56cfbc3567936c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-05 06:05:44 -0600

    css works

--- index.js
+++ index.js
@@ -11,22 +11,26 @@
         - Summary
         - Seasons listed as individual divs
 */
-async function findShow(query){
+
+async function findShow(query) {
     let response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`)
     let data = await response.json()
     return data
 }
 
-findShow('friends').then(show =>{
+findShow("office").then(show => {
     console.log(show)
-    document.body.innerHTML = `<div class = "the-show">
-    <div class = "show-title">${show.name}</div>
-    <div class = "show-summary">${show.summary}</div>
-     ${show._embedded.seasons.map(season =>{
-         return `<div class ="show-season"> Season ${season.number}</div>`}).join('')
-         
-         }
-         
-    </div>
-    `
+    document.body.innerHTML = `<div class="my-show">
+        <div class="my-show-title">
+            ${show.name}
+        </div>
+        
+        <div class="my-show-summary">
+            ${show.summary}
+        </div>
+        
+        ${show._embedded.seasons.map(season => {
+            return `<div class="my-show-season">Season ${season.number}</div>`
+        }).join('')}
+    </div>`
 })
